986fcd161e1122cea3d03b5acdf4d7b94584cd9c
sarangmech07 <45466323+sarangmech07@users.noreply.github.com>
2019-01-04 17:36:28 +0530

    Adding Tests1

---
+++ Test Suites/CalculatorTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CalculatorTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8aaf06a5-9cf9-4a61-a956-5ec0f2879c12</testSuiteGuid>
+   <testCaseLink>
+      <guid>c05103e8-c8d9-44fb-81c9-c5b06806d422</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
